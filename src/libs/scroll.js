e2032e086d62c33b486013c2c68459585ca04150
lepa15 <lepa2604@gmail.com>
2023-03-06 16:47:35 +0200

    features block

--- src/libs/scroll.js
+++ src/libs/scroll.js
@@ -1,3 +1,5 @@
+//* Анимация для header 
+
 let minOffset = 67; //670
 
 window.onscroll = function () {
@@ -13,3 +15,28 @@ window.onscroll = function () {
 
 	}
 }
+
+
+//* CSS-анимация при прокрутке страницы
+const description = document.querySelector('.features-block__description');
+const messageWrapper = document.querySelector('.features-block__message-wrapper');
+const messages = document.querySelectorAll('.features-block__message');
+const featuresMainContentWrapper = document.querySelector('.features-block__main-content-wrapper');
+const featuresSecondaryItems = document.querySelectorAll('.features-block__secondary-item');
+ 
+
+
+const observer = new IntersectionObserver(entries => {
+	entries.forEach(entry => {
+		if (entry.isIntersecting) {
+			entry.target.classList.add('animation');
+			return
+		}
+	})
+})
+
+observer.observe(description);
+observer.observe(messageWrapper);
+observer.observe(featuresMainContentWrapper);
+messages.forEach(message => observer.observe(message))
+featuresSecondaryItems.forEach(item => observer.observe(item))
